7fd9e117ee988e69d0e0cc939f4b56df2d624c3b
dominik985 <dominik_pk@interia.pl>
2016-12-20 23:17:00 +0100

    Login form layout added and some changes to base layout

--- src/AppBundle/Form/CommentType.php
+++ src/AppBundle/Form/CommentType.php
@@ -17,7 +17,7 @@ class CommentType extends AbstractType
     {
         $builder->add('content', TextareaType::class, array(
             'label' => false,
-            'attr' => array('placeholder' => 'Add comment')
+            'attr' => array('placeholder' => 'Content of comment')
         ));
     }
     
